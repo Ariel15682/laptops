2569d4f3166e1c7bbd239db8552e2560c5744395
ariel <arieldiaz05@gmail.com>
2023-04-12 09:50:59 +0200

    Modificaciones en archivo de propiedades

--- src/main/java/com/example/controller/HelloControllers.java
+++ src/main/java/com/example/controller/HelloControllers.java
@@ -1,11 +1,15 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloControllers {
 
+    @Value("${app.message}")
+    private String mensaje;
+
     @GetMapping("/saludo")
-    public String saludo(){ return "Hola, buenos dias desde Spring!";}
+    public String saludo(){ return "Hola, buenos dias desde: " + mensaje;}
 }
